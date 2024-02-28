adf82696f255b59955f4935115a19f2dcc7f7090
datttpc02990 <117361057+datttpc02990@users.noreply.github.com>
2024-02-28 15:53:07 +0700

    [ProjectType]- quản lý dự án

---
+++ laravel/database/seeders/ProjectTypeSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\ProjectType;
+class ProjectTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $projectTypes = [
+            [
+                'name' => 'Dự án 1',
+                'description' => 'Description of Type 1',
+                'status' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Dự án mẫu',
+                'description' => 'Description of Type 2',
+                'status' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Dự án tốt nghiệp',
+                'description' => 'Description of Type 2',
+                'status' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Dự án thực tập',
+                'description' => 'Description of Type 2',
+                'status' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Dự án xã họi',
+                'description' => 'Description of Type 2',
+                'status' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Thêm các dòng dữ liệu khác nếu cần
+
+        ];
+
+        // Thêm dữ liệu vào bảng project_types
+        ProjectType::insert($projectTypes);
+    }
+}
