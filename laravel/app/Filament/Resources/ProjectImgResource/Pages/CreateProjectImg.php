fac2cc609c719cd2153533deebb4d4e390d84717
datttpc02990 <117361057+datttpc02990@users.noreply.github.com>
2024-02-28 15:55:56 +0700

    [Resource] - Project - Quản lý hình ảnh

---
+++ laravel/app/Filament/Resources/ProjectImgResource/Pages/CreateProjectImg.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ProjectImgResource\Pages;
+
+use App\Filament\Resources\ProjectImgResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProjectImg extends CreateRecord
+{
+    protected static string $resource = ProjectImgResource::class;
+}
