6082473c1f1707c34350fa2c5aa8fa79ab9081b8
datttpc02990 <117361057+datttpc02990@users.noreply.github.com>
2024-02-28 16:30:13 +0700

    [ Models]- bổ sung Semester( thêm mói quan hệ cho link)

--- laravel/app/Models/Semester.php
+++ laravel/app/Models/Semester.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Semester extends Model
 {
@@ -16,4 +17,7 @@ class Semester extends Model
         'created_at',
         'updated_at',
     ];
+    public function link():HasMany{
+        return $this->hasMany(Link::class);
+    }
 }
