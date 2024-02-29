100e48209ae9560f264aaf2027f0113e694261aa
TruongVicent <110811587+TruongVicent@users.noreply.github.com>
2024-02-29 12:05:04 +0700

    resource event

---
+++ laravel/app/Filament/Resources/EventUserResource/Pages/CreateEventUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\EventUserResource\Pages;
+
+use App\Filament\Resources\EventUserResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEventUser extends CreateRecord
+{
+    protected static string $resource = EventUserResource::class;
+}
